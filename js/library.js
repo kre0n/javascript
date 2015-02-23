773845a3091653984a6c04d826142da06fadcafe
kre0n <kreon@ya.ru>
2015-02-24 00:15:48 +0300

    2

--- js/library.js
+++ js/library.js
@@ -1,15 +1,74 @@
 (function() {
     window.library = function() {
-        return {
+        var result;    
+        
+        /**
+         * Супер класс для всех элементов
+         * 
+         * @returns {library.Element}
+         */
+        result.Element = function() {
+            
+        };
+        result.Element.prototype = {
+            
             /**
-             * Выпадающий список
              * 
+             * @returns {undefined}
              */
-            combobox : (function() {
+            addEvent : function( name, handler ) {
                 
+            },
+            
+            /**
+             * 
+             * @param {type} name
+             * @param {type} handler
+             * @returns {undefined}
+             */
+            removeEvent : function( name, handler ) {
                 
+            },
+            
+            /**
+             * 
+             * @param {type} descriptor
+             * @returns {undefined}
+             */
+            addEvents : function( descriptor ) {
                 
-            })(),
-        }
+            },
+            
+            /**
+             * 
+             * @param {type} descriptor
+             * @returns {undefined}
+             */
+            removeEvents : function( descriptor ) {
+                
+            },
+            
+            /**
+             * 
+             * @returns {undefined}
+             */
+            raiseEvent : function( name ) {
+                
+            },
+        };
+        
+        /**
+         * Выпадающий список
+         * 
+         * @returns {undefined}
+         */
+        result.Combobox = function() {
+            
+        };
+        result.Combobox.prototype = {
+            
+        };
+                
+        return result;
     };
 })();
