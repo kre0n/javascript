0864dc313db2f7ba12cfe4e0ea5d25103c44d9a4
kre0n <kreon@ya.ru>
2015-03-01 23:20:40 +0300

    +1

--- js/library.js
+++ js/library.js
@@ -1,86 +1,128 @@
 (function() {
     window.library = (function() {
-        var library = {};
-        
-        library.HTML_BODY_STRING = 'HTMLBodyElement';
-        library.ARRAY_STRING = 'Array';
-        
-        library.codes = {
-            faledExecuteArguments : 'Ошибка при выходе функции %0 в %1: %2 аргументов ожидалось, а получено %3.',
-            wrongTypeOfVariable : 'Неверный тип переменной %0. Ожидалося тип - %1',
-            errorCodeNotFound : 'В библиотеке не найден код ошибки. Код - %0',
+        // сразу опрделяем статические методы, и константы
+        var library = {
+            // типы для встроенных объектов js
+            TYPES : {
+                HTML_BODY_STRING : 'HTMLBodyElement',
+                ARRAY_STRING : 'Array',
+                OBJECT_STRING : 'Object',
+            },
             
-        };
-        
-        /**
-         * работет быстрее чем обычный arr.forEach()
-         * 
-         * @param {type} arr
-         * @param {type} fn
-         * @returns {undefined}
-         */
-        library.forEach = function( arr, fn ) {
+            // константы - для ошибок
+            codes : {
+                faledExecuteArguments : 'Ошибка при выполнении функции %0 в %1: %2 аргументов ожидалось, а получено %3.',
+                wrongTypeOfVariable : 'Неверный тип переменной %0. Ожидался тип - %1',
+                errorCodeNotFound : 'В библиотеке не найден код ошибки. Код - %0',
+
+            },
+
+            /**
+             * работет быстрее чем обычный arr.forEach()
+             * 
+             * @param {type} arr
+             * @param {type} fn
+             * @returns {undefined}
+             */
+            forEach : function( arr, fn ) {
+
+                if( !library.isArray( arr ) ) library.throwError( 'wrongTypeOfVariable', [ 'arr', 'Массив' ] );
+                var len = arr.length;
+                for( var i = 0; i < len; i++ ) {
+                    fn( arr[i], i );
+                }
+            },
+
+            /**
+             * 
+             * 
+             * @param {string} type
+             * @param {array} codes
+             * @returns {undefined}
+             */
+            throwError : function( type, codes ) {
+
+                codes = codes || [];
+                var string = library.codes[ type ];
+                if( !string ) library.throwError( 'errorCodeNotFound', [ type ] );
+
+                library.forEach( codes, function( item, i ) {
+                    string = string.replace( '%' + i, item );
+                } );
+
+                throw new Error( string );            
+            },
+
+            /**
+             * @static 
+             * проверяет является ли аргумент узлом-элементом
+             * 
+             * @param {HTMLElement} item узел элемент
+             * @returns {boolean}
+             */
+            isHTMLElement : function( item ) {
+                var string = {}.toString.call( item );
+                if( ~string.indexOf( library.TYPES.HTML_BODY_STRING ) ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+
+            /**
+             * @static 
+             * проверяет является ли аргумент массивом
+             * 
+             * @param {type} arr
+             * @returns {Boolean}
+             */
+            isArray : function( arr ) {
+                var string = {}.toString.call( arr );
+                if( ~string.indexOf( library.TYPES.ARRAY_STRING ) ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+
+            /**
+             * @static 
+             * проверяет является ли аргумент объектом
+             * 
+             * @param {type} arr
+             * @returns {Boolean}
+             */
+            isObject : function( arr ) {
+                var string = {}.toString.call( arr );
+                if( ~string.indexOf( library.TYPES.OBJECT_STRING ) ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
             
-            if( !library.isArray( arr ) ) library.throwError( 'wrongTypeOfVariable', [ 'arr', 'Массив' ] );
-            var len = arr.length;
-            for( var i = 0; i < len; i++ ) {
-                fn( arr[i], i );
-            }
-        };
-        
-        /**
-         * 
-         * 
-         * @param {string} type
-         * @param {array} codes
-         * @returns {undefined}
-         */
-        library.throwError = function( type, codes ) {
-            
-            codes = codes || [];
-            var string = library.codes[ type ];
-            if( !string ) library.throwError( 'errorCodeNotFound', [ type ] );
+            /**
+             * Создает и возвращает HTML элемент
+             *
+             * @param {type} tagName
+             * @param {type} descriptor
+             * @returns {HTMLElement}
+             */
+            newTag : function( tagName, descriptor ) {
                 
-            library.forEach( codes, function( item, i ) {
-                string = string.replace( '%' + i, item );
-            } );
-            
-            throw new Error( string );            
+            },
         };
-        
-        /**
-         * @static 
-         * проверяет является ли аргумент узлом-элементом
-         * 
-         * @param {HTMLElement} item узел элемент
-         * @returns {boolean}
-         */
-        library.isHTMLElement = function( item ) {
-            var string = {}.toString.call( item );
-            if( ~string.indexOf( library.HTML_BODY_STRING ) ) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        
-        library.isArray = function( arr ) {
-            var string = {}.toString.call( arr );
-            if( ~string.indexOf( library.ARRAY_STRING ) ) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        
+            
         /**
          * Супер класс для всех элементов
          * 
          * @returns {library.Element}
          */
         library.LibElement = ( function() {
-            var c = function() {
-                console.log( 'конструктор LibElem');
+            var c = function( descriptor ) {
+                
+                library.newTag();
+                
             };
             
             c.prototype = Object.create( Object.prototype );
